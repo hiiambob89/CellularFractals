38d0a145c2f8c4052cefff3df1fde8b411a9bfc6
Kaden Hart <kadenhart100@gmail.com>
2025-03-01 04:05:49 -0700

    added color variation

--- app/src/main/java/cellularfractals/particles/particles/GravityParticle.java
+++ app/src/main/java/cellularfractals/particles/particles/GravityParticle.java
@@ -9,13 +9,16 @@ import cellularfractals.particles.effects.NegativeGravityEffect;
 public class GravityParticle extends BasicParticle {
   public GravityParticle(World world, double x, double y, double dx, double dy, Float gravitRange, Float gravityStrength) {
     super(world, x, y, dx, dy);
-    this.cosmeticSettings.color = new Color(255,0,255);
 
     // Simply choose which effect to add based on the sign of gravityStrength
     if (gravityStrength < 0) {
       this.addEffect(new NegativeGravityEffect(gravitRange, gravityStrength));
+    this.cosmeticSettings.color = new Color(255,0,175);
+
     } else {
       this.addEffect(new GravityEffect(gravitRange, gravityStrength));
+    this.cosmeticSettings.color = new Color(175,0,255);
+
     }
   }
 }
